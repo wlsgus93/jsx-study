48d42c3f46b56ee6de770f80e91eeab1edc738f0
김진현 <wlsgus63@naver.com>
2025-07-30 10:57:55 +0900

    [init] 프로젝트 초기화 및 생성 및 기본 틀 작성(Card)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+
+function App() {
+  const name:string = "jhk";
+  return (
+    <>
+      <div className="card">
+        <img src="/avatar.png" alt="avatar" />
+        <h2>{name}</h2>
+        <h2>Programmer</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
